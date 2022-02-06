f268eb1f4bd56ef2be5f5c82d6470255e998614e
migguelch <migguelch@gmail.com>
2022-02-06 07:21:44 -0500

    public blueprint
    The public Blueprint was created.

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,10 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+
+    #BluePrints
+    from .public import public_Blueprint
+    app.register_blueprint(public_Blueprint)
+
+
     return app
